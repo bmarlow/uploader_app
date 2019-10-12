048b3a5aa01a19c7bb6feaad5b104edaa0f85c68
Unknown <brandon.n.marlow@gmail.com>
2019-10-12 15:18:08 -0700

    remove message function :-/

--- main.py
+++ main.py
@@ -13,17 +13,6 @@ def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-def fire_kafka_producer_log(filename):
-    logging.info('kafka function entered')
-    producer = kafka.KafkaProducer(bootstrap_servers='my-cluster-kafka-bootstrap:9092')
-    logging.info('kafka producer defined')
-    enc_message = bytes('this is a test message that ' + filename + ' was received', encoding='utf-8')
-    logging.info('message encoded, message is: ' + bytes.decode(enc_message))
-    producer.send('file-received', enc_message)
-    logging.info('message sent')
-    return flash('log sent')
-
-
 @app.route('/')
 def upload_form():
     return render_template('upload.html')
@@ -41,7 +30,8 @@ def upload_file():
             if file and allowed_file(file.filename):
                 filename = secure_filename(file.filename)
                 file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-                fire_kafka_producer_log(file.filename)
+                enc_message = bytes('The following file was received: ' + file.filename + '')
+                producer.send('file-received', enc_message )
         flash('File(s) successfully uploaded')
         return redirect('/')
 
@@ -51,9 +41,9 @@ def stage_files():
     if request.method == 'POST':
         shutil.move("/root/data/X.npy", "/root/uploads/X.npy")
         shutil.move("/root/data/y.npy", "/root/uploads/y.npy")
-        fire_kafka_producer_log('X.npy')
-        fire_kafka_producer_log('y.npy')
-        producer.send('file-received', b'this is a test')
+        producer.send('file-received', b'The following file was received: X.npy')
+        producer.send('file-received', b'The following file was received: y.npy')
+
         flash('Staging files used')
         return redirect('/')
 
@@ -84,13 +74,6 @@ def reset_stage():
     return redirect('/')
 
 
-@app.route("/test", methods=['GET'])
-def kafka_producer_test():
-    if request.method == 'GET':
-        fire_kafka_producer_log('testfilename')
-    return redirect('/')
-
-
 if __name__ == "__main__":
     producer = kafka.KafkaProducer(bootstrap_servers='my-cluster-kafka-bootstrap:9092')
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0')
