c24519089e2fb088eb5fc3ba599a00e9370f482d
Unknown <brandon.n.marlow@gmail.com>
2019-10-13 15:17:17 -0700

    add files in the right spot

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import os
 # import magic
 import urllib.request
-import shutil, kafka, logging
+import shutil, kafka, logging, subprocess
 from app import app
-from flask import Flask, flash, request, redirect, render_template, abort, send_file, jsonify, subprocess
+from flask import Flask, flash, request, redirect, render_template, abort, send_file, jsonify
 from werkzeug.utils import secure_filename
 
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'npy'])
