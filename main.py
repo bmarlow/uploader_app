f8a72ffd4fbf787733b7f11bdd8261d9ab58a8f7
Brandon Marlow <brandon.n.marlow@gmail.com>
2019-10-15 18:18:52 -0400

    add try statement

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ def upload_file():
             if file and allowed_file(file.filename):
                 filename = secure_filename(file.filename)
                 file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-                enc_message = bytes('Thae following file was received: ' + file.filename + '', encoding='utf-8')
+                enc_message = bytes('The following file was received: ' + file.filename + '', encoding='utf-8')
                 producer.send('file-received', enc_message)
         flash('File(s) successfully uploaded')
         return redirect('/')
