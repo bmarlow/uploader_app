fabe5cd4caa92c7ca45604621b8592170c7f1d3e
Brandon Marlow <brandon.n.marlow@gmail.com>
2019-10-16 02:19:40 -0400

    general cleanup

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ def upload_file():
             if file and allowed_file(file.filename):
                 filename = secure_filename(file.filename)
                 file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-                enc_message = bytes('The following file was received: ' + file.filename + '', encoding='utf-8')
+                enc_message = bytes('DROPOFF POD (AMQ): The following file was received: ' + file.filename + '', encoding='utf-8')
                 producer.send('file-received', enc_message)
             else:
                 return render_template('naughty.html')
@@ -101,22 +101,22 @@ def download_processed_files(path):
 def api_upload_file():
     # check if the post request has the file part
     if 'file' not in request.files:
-        resp = jsonify({'message' : 'No file part in the request'})
+        resp = jsonify({'message': 'No file part in the request'})
         resp.status_code = 400
         return resp
     file = request.files['file']
     if file.filename == '':
-        resp = jsonify({'message' : 'No file selected for uploading'})
+        resp = jsonify({'message': 'No file selected for uploading'})
         resp.status_code = 400
         return resp
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
         file.save(os.path.join('/tmp/processed', filename))
-        resp = jsonify({'message' : 'File successfully uploaded'})
+        resp = jsonify({'message': 'File successfully uploaded'})
         resp.status_code = 201
         return resp
     else:
-        resp = jsonify({'message' : 'Allowed file types are txt, pdf, png, jpg, jpeg, gif'})
+        resp = jsonify({'message': 'This was not an approved file extension.'})
         resp.status_code = 400
         return resp
 
