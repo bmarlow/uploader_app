378b1f3e911bf3a90372fdce9a9123fe7a8aff53
Brandon Marlow <brandon.n.marlow@gmail.com>
2019-10-16 21:35:22 -0400

    life is hard when you're dumb

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ def upload_file():
             if file and allowed_file(file.filename):
                 filename = secure_filename(file.filename)
                 file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-                enc_message = bytes('DROPOFF POD (AMQ): The following file was received: ' + file.filename + '', encoding='utf-8')
+                enc_message = bytes('DROPOFF POD (AMQ)-- The following file was received: ' + file.filename + '', encoding='utf-8')
                 producer.send('file-received', enc_message)
             else:
                 return render_template('naughty.html')
